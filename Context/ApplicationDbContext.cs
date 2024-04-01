813afc6f953f6f34fc5e8b974529fe106e5965ec
mohan-nallaian <161158819+mohan-nallaian@users.noreply.github.com>
2024-04-01 12:44:18 +0530

    Add files via upload

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeCrud.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeCrud.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        { 
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Data/EmployeeService.cs
@@ -0,0 +1,51 @@
+﻿using EmployeeCrud.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+
+namespace EmployeeCrud.Data
+{
+    public class EmployeeService
+    {
+        public readonly ApplicationDbContext _applicationDbContext;
+
+        public EmployeeService(ApplicationDbContext applicationDbContext) 
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<List<Employee>> GetAllEmployees() 
+        {
+            return await _applicationDbContext.Employees.ToListAsync();
+        }
+    
+        public async Task<bool> InsertEmployee(Employee employee)
+        {
+            await _applicationDbContext.Employees.AddAsync(employee);
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+        public async Task<Employee> GetEmployeeById(int id)
+        {
+            Employee employee = await _applicationDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+            return employee;
+        }
+
+        public async Task<bool> UpdateEmployeeDetails(Employee employee)
+        {
+            _applicationDbContext.Employees.Update(employee);
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> DeleteEmployee(Employee employee)
+        {
+            _applicationDbContext.Employees.Remove(employee);
+            await _applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+
+
+
+    }
+}
